8a5928daa3d675671fe1709b53295e939c09d771
Mishin Yura <mishin.inbox@gmail.com>
2022-09-10 13:34:06 +0300

    Add Hello, World! Closes #1

---
+++ 1_helloworld/firstapp.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var phrase string
+	phrase = "Hello, String!"
+	var s = "Hello, S!"
+	s2 := "Hello, S2"
+
+	fmt.Println("Hello, World!")
+	fmt.Println(phrase)
+	fmt.Println(s)
+	fmt.Println(s2)
+
+	// ждать пока не юзер не нажмет enter
+	//fmt.Scanf("h")
+}
